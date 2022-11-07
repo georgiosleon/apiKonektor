24d8a8aea5d1f4be5da35fb98853b2ffbe8de2ac
George Leon <george.leon.external@atos.net>
2022-11-07 10:51:36 +0200

    Fix logging with tinylog  on start up print  ok messages  for log levels .

--- src/main/java/leonsolutions/apiKonektor/ApiKonektorApplication.java
+++ src/main/java/leonsolutions/apiKonektor/ApiKonektorApplication.java
@@ -2,12 +2,21 @@ package leonsolutions.apiKonektor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.tinylog.Logger;
+import org.tinylog.TaggedLogger;
 
 @SpringBootApplication
 public class ApiKonektorApplication {
 
 	public static void main(String[] args) {
+
+		TaggedLogger logger = Logger.tag("  ApiKonektorApplication - (c) 2022 ");
+		logger.warn  ("@@@@@@@@@@@");
+		logger.debug ("@@@@@@@@@@@");
+		logger.error ("@@@@@@@@@@@");
 		SpringApplication.run(ApiKonektorApplication.class, args);
+		logger.info  ("@@@@@@@@@@@");
+		logger.info  ("All ok. system started... ");
 	}
 
 }
